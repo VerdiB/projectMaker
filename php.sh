ac274b556cde177f72d6b2d7f4c50d1d89550cf8
VerdiB <verdiburgersjr@hotmail.com>
2023-09-27 20:43:55 +0200

    php module added with MVC model startup and a filled-startup index

--- php.sh
+++ php.sh
@@ -2,6 +2,31 @@
 
 projectName=$1
 
-mkdir -p $projectName/src/Model $projectName/src/View $projectName/src/Controller
-touch $projectName/src/index.php $projectName/src/db.sql
-# fill index.php with standard values like head, title and such 
+mkdir -p $projectName/src/Model $projectName/src/View $projectName/src/Controller $projectName/src/Css
+touch $projectName/src/index.php $projectName/src/db.sql $projectName/src/Css/style.css
+
+# making a basic index setup with just the html tags 
+index_content=$(cat << 'EOF'
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Css/style.css"/>
+        <title>PLACEHOLDER_PROJECTNAME</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+            </nav>
+        </header>
+        <main>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
+EOF
+)
+index_content="${index_content/PLACEHOLDER_PROJECTNAME/projectName}"
+echo "$index_content" >$projectName/src/index.php
